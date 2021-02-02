d4941918733c823fa3d424607dad5f347f7324f2
Maximilien BEY <bey.maximilien@gmail.com>
2021-02-02 03:08:28 +0100

    refactoring

--- src/js/scriptPage5.js
+++ src/js/scriptPage5.js
@@ -14,53 +14,64 @@ window.addEventListener('load', () => {
     let enigma = riddles[Math.round(Math.random() * (riddles.length - 1))]
 
     document.querySelector('#admin').addEventListener('click', () => {
-
+        let check = prompt(enigma.question)
+        if(check.toLowerCase() === enigma.answer.toLowerCase()) {
+            document.querySelector('img').src = "/img/map1.jpg"
+            document.querySelector('#admin').remove()
+            //redirect
+        } else {
+            counter--
+            //afficher erreur
+        }
+        if(!counter) {
+            alert("L'imposteur vous a tué.")
+        }
     })
 })
-function puzzleFonction() {
+// function puzzleFonction() {
     
-    //Une liste des localisations de la carte de Amoung Us.
-    var locations = ["Admin", "Cafeteria", "Comminications", "Electrical", "Lower Engine", "Navigation", "MedBay", "O2", "Reactor", "Security", "Shields", "Storage", "Weapons"];
+//     //Une liste des localisations de la carte de Amoung Us.
+//     var locations = ["Admin", "Cafeteria", "Comminications", "Electrical", "Lower Engine", "Navigation", "MedBay", "O2", "Reactor", "Security", "Shields", "Storage", "Weapons"];
     
-    //Le MDP à trouver.
-    var password = "Avenir";
+//     //Le MDP à trouver.
+//     var password = "Avenir";
     
-    //Un conteur pour savoir combien d'essaye il nous reste encore.
-    //Note: Je ne sais pas si on va l'utiliser.
-    var counter = 3;
+//     //Un conteur pour savoir combien d'essaye il nous reste encore.
+//     //Note: Je ne sais pas si on va l'utiliser.
+//     var counter = 3;
     
-    //Un texte pour afficher la réponse.
-    var text_resultat;
+//     //Un texte pour afficher la réponse.
+//     var text_resultat;
 
-    //NOus entrons enfin dans l'utilité de la function, on demande de trouver le MDP caché.
-    var find_password = prompt("Je suis toujours devant tout le monde, mais personne ne peut me voir. Qui suis-je ?");
+//     //NOus entrons enfin dans l'utilité de la function, on demande de trouver le MDP caché.
+//     var find_password = prompt("Je suis toujours devant tout le monde, mais personne ne peut me voir. Qui suis-je ?");
 
-    //On a plusieurs conditions.
-    //La première permet de savoir si l'utilisateur à trouvé le mot de passe.
-    if (find_password == password) {
+//     //On a plusieurs conditions.
+//     //La première permet de savoir si l'utilisateur à trouvé le mot de passe.
+//     if (find_password == password) {
         
-        //Pour le 
-        //text_resultat = "Hein, mais comment vous m'avez trouvé? Vous êtes vraiment très perspicace!";
-        document.body.style.backgroundImage = "url(../img/map1.jpg)";
-        document.getElementById("test").remove();
-    } 
+//         //Pour le 
+//         //text_resultat = "Hein, mais comment vous m'avez trouvé? Vous êtes vraiment très perspicace!";
+//         document.body.style.backgroundImage = "url(../img/map1.jpg)";
+//         document.getElementById("test").remove();
+//     } 
     
-    if (find_password != password) {
+//     if (find_password != password) {
         
-        text_resultat = "Non c'est pas ça, il vous reste " + counter + " tentative.";
+//         text_resultat = "Non c'est pas ça, il vous reste " + counter + " tentative.";
 
-    }
+//     }
 
-    if (find_password == "") {
+//     if (find_password == "") {
 
-        text_resultat = "";
-    }
+//         text_resultat = "";
+//     }
 
-    if (find_password != password && counter == 0) {
+//     if (find_password != password && counter == 0) {
         
-        text_resultat = "L'imposteur vous a tuer";
-    }
+//         text_resultat = "L'imposteur vous a tuer";
+//     }
 
-    //document.getElementById("script").innerHTML = text_resultat;
+//     //document.getElementById("script").innerHTML = text_resultat;
 
-}
+// }
