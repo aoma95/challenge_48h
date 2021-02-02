50d84b0ca89ce07444f086ccadae24fd50fead83
Maximilien BEY <bey.maximilien@gmail.com>
2021-02-02 04:36:56 +0100

    finish

--- src/js/page9.js
+++ src/js/page9.js
@@ -3,7 +3,7 @@ const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
 window.addEventListener('load', async () => {
     await sleep(1000)
     Array.from(document.querySelectorAll('.interact')).map(element => element.style.visibility = "visible")
-    document.querySelector('img').src = "/img/win.jpg"
+    document.querySelector('img').src = "/img/defeat.jpg"
     document.querySelector('#replay').addEventListener('click', () => {
         alert("replay")
     })
